122b92a565d361b15d0779848e343f331ab0552a
Usukhbayar <osoho_123@yahoo.com>
2023-03-31 16:52:25 +0800

    ..

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { NotFoundScreen } from "./screens/NotFoundScreen";
-import { SignIn, SignInScreen } from "./screens/SignInScreen";
-import { SignUp, SignUpScreen } from "./screens/SignUpScreen";
+import { SignInScreen } from "./screens/SignInScreen";
+import { SignUpScreen } from "./screens/SignUpScreen";
 import { useCurrentUser } from "./hooks/useCurrentUser";
 
 function App() {
   const { currentUser } = useCurrentUser();
-  if (currentUser) {
+  if (!currentUser) {
     return (
       <Routes>
         <Route path="*" element={<NotFoundScreen />} />
