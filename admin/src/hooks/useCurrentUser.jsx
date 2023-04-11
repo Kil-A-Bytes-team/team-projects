b7b222664043ed596427de953a692e8bcee48db9
Kazugatt <116425323+Kazugatt@users.noreply.github.com>
2023-04-11 17:59:38 +0800

    Home page

--- admin/src/hooks/useCurrentUser.jsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { CurrentUserContext } from "../contexts/CurrentUserProvider";
-
-export const useCurrentUser = () => {
-  const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
-  return { currentUser, setCurrentUser };
-};
