2f8cb7439cc4b1493da2498a76d52c7bac3c61bf
Usukhbayar <osoho_123@yahoo.com>
2023-04-19 15:59:53 +0800

    added frontend

--- server-nest-js/src/main.ts
+++ server-nest-js/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { env } from 'process';
 import { AppModule } from './app.module';
 
-
+// hi 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(8000);
