30ce91237dbb1b34f16927c55f7a69274b5a7b92
Usukhbayar <osoho_123@yahoo.com>
2023-04-20 12:59:50 +0800

    ..

--- server-nest-js/src/app.controller.ts
+++ server-nest-js/src/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { Secured } from './auth/secured.decorator';
+// import { Secured } from './auth/secured.decorator';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Secured('admin')
+  // @Secured('admin')
   getHello(): string {
     return this.appService.getHello();
   }
